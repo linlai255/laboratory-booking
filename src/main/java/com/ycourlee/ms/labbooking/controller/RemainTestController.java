6114ee85b5c41b8870028a4101bee00c26983b79
yalden <ycourlee@qq.com>
2021-05-31 12:40:07 +0800

    UPDATE API and ignore test controller apis.

--- src/main/java/com/ycourlee/ms/labbooking/controller/RemainTestController.java
+++ src/main/java/com/ycourlee/ms/labbooking/controller/RemainTestController.java
@@ -4,10 +4,12 @@ import com.ycourlee.root.core.domain.context.ApiResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * @author yongjiang
  */
+@ApiIgnore
 @RestController
 @SuppressWarnings("rawtypes")
 public class RemainTestController {
